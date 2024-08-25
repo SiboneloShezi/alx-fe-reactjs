c28c11227641690773a02a3a730f6f215acda7b8
SiboneloShezi <wizardshezi@gmail.com>
2024-08-25 19:17:11 +0200

    updated details

--- alx-react-app-props/src/ProfilePage.jsx
+++ alx-react-app-props/src/ProfilePage.jsx
@@ -1,13 +1,7 @@
-import React from 'react';
+import UserInfo from './UserInfo';
 
-function UserProfile(props) {
-  return (
-    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px', borderRadius: '5px' }}>
-      <h2 style={{ color: 'blue', fontSize: '24px' }}>{props.name}</h2>
-      <p>Age: <span style={{ fontWeight: 'bold', color: 'darkgreen' }}>{props.age}</span></p>
-      <p>Bio: {props.bio}</p>
-    </div>
-  );
+function ProfilePage({ userData }) {
+  return <UserInfo userData={userData} />;
 }
 
-export default UserProfile;
+export default ProfilePage;
