9152278b38b4f38582c0a0ee88ddac714e543b2d
SiboneloShezi <wizardshezi@gmail.com>
2024-08-25 17:04:31 +0200

    updated file names

--- alx-react-app-props/alx-react-app copy/src/App.jsx
+++ alx-react-app-props/alx-react-app copy/src/App.jsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import ProfilePage from "./components/ProfilePage";
-import UserContext from "./components/UserContext";
+import UserContext from "./UserContext";
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
