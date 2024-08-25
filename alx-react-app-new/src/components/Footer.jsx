1f5a0c27b987ca01ca1d2d2e5544f1ef28129ebc
SiboneloShezi <wizardshezi@gmail.com>
2024-08-25 18:59:48 +0200

    deleted a duplicated folder for props

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer
+      style={{
+        padding: "20px",
+        backgroundColor: "#f4f4f4",
+        textAlign: "center",
+        borderTop: "1px solid #ccc",
+      }}
+    >
+      <p style={{ margin: 0 }}>© 2024 My Favorite Cities</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#f4f4f4', textAlign: 'center', borderTop: '1px solid #ccc' }}>
+      <h2 style={{ color: 'darkslategray' }}>Main Content</h2>
+      <p style={{ lineHeight: '1.6' }}>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
