b57120dba909f5f1153d3650b1ce5997bca1afd4
SiboneloShezi <wizardshezi@gmail.com>
2024-08-25 15:46:20 +0200

    changed main file paragraph

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -4,7 +4,7 @@ function MainContent() {
   return (
     <main style={{ padding: '20px', backgroundColor: '#f4f4f4', textAlign: 'center', borderTop: '1px solid #ccc' }}>
       <h2 style={{ color: 'darkslategray' }}>Main Content</h2>
-      <p style={{ lineHeight: '1.6' }}>Here is the main content area.</p>
+      <p style={{ lineHeight: '1.6' }}>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
 }
